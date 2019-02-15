94453c1e3ae18deba11a07ce78852f7390cf9de3
shotaro-ooe <shoooe97@gmail.com>
2019-02-15 14:27:51 +0900

    NCMB取得、読み込み成功した

---
+++ Assets/NCMB/Script/DataFetch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+
+public class DataFetch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //スコアが１０より大きいプレイヤーデータを取得する
+        FetchScoreList(10);
+	}
+	
+    private void FetchScoreList(int higherThan){
+
+        //複数のNCMBObjectを取得するクエリを生成
+        NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("PlayerData");
+
+        //条件設定
+        query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
+        {
+            if (e != null)
+            {
+                //エラー処理
+            }
+            else
+            {
+                //成功時の処理
+                foreach (NCMBObject obj in objList)
+                {
+                    Debug.Log("PlayerName:" + obj["PlayerName"] + ",Score:" + obj["Score"] + ",Coin:" + obj["Coin"]);
+                }
+            }
+        });
+    }
+}
---
+++ Assets/NCMB/Script/DataSave.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+
+public class DataSave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SavePlayerData("AAA", 100, 300);
+	}
+	
+    public void SavePlayerData(string playerName,int score,int coin){
+        NCMBObject obj = new NCMBObject("PlayerData");
+
+        obj.Add("PlayerName", playerName);
+        obj.Add("Score", score);
+        obj.Add("Coin", coin);
+
+        obj.SaveAsync((NCMBException e) =>
+        {
+            if (e != null)
+            {
+                //エラー処理
+                Debug.Log("保存失敗");
+            }
+            else
+            {
+                //保存成功
+                Debug.Log("保存成功");
+
+            }
+        });
+    }
+}
